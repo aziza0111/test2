198d45eaf84bc869120b5f53800089abb986d845
aziza0111 <azizamaksatbekkyzy8@gmail.com>
2024-04-26 20:14:01 +0600

    test2

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// // let может видеть значение
+// let a =  'a'
+// console.log(a)
+// a='d'
+// console.log(a)
+
+//  if(true){
+//     let b = 'f' //имеет область видимости()
+//  }
+//  console.log(b)
+// console.log(a)
+// const a = 'a'
+// a =  'b'
+// console.log(a)
+
+// if(true){
+//     const b = 'k'
+// }
+// console.log(b)
+
+// const obj = {
+//     name: 'Ilya'
+// }
+
+// // obj = { name: John}
+// console. log (obj)
+
+// const arr =[ 1, 2, 3 , 4, 5]
+
+// arr.pop()
+// console.log(arr)
+
+// //const {}  [] /html el
+// // let - for/str/  num/ bool
+
+// function myFunc(){
+    
+// }
+
+const myFunc = () => 'a'
+console.log(myFunc())
